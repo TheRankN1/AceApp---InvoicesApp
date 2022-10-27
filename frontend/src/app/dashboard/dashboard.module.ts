fb060fa44d5858395d63d7b702f009ae90c7fc98
TheRankN1 <petrutpoenaru@yahoo.com>
2022-10-27 18:14:01 +0300

    Added DashBoard ROUTES

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -4,13 +4,27 @@ import { TopbarComponent } from './components/topbar/topbar.component';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutesModule } from './dashboard.routes';
 import { SidenavComponent } from './components/sidebar/sidenav.component';
+import { UserComponent } from './views/user/user.component';
+import { ClientsComponent } from './views/clients/clients.component';
+import { CompaniesComponent } from './views/companies/companies.component';
+import { InvoicesComponent } from './views/invoices/invoices.component';
+import { OffersComponent } from './views/offers/offers.component';
+import { LogsComponent } from './views/logs/logs.component';
+import { ConfigComponent } from './views/config/config.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const COMPONENTS = [TopbarComponent, DashboardComponent, SidenavComponent];
+const COMPONENTS = [
+  TopbarComponent,
+  DashboardComponent,
+  SidenavComponent,
+  UserComponent,
+  ClientsComponent,
+];
 
 const MODULES = [CommonModule, DashboardRoutesModule];
 
 @NgModule({
-  declarations: [COMPONENTS],
+  declarations: [COMPONENTS, CompaniesComponent, InvoicesComponent, OffersComponent, LogsComponent, ConfigComponent, FooterComponent],
   imports: [MODULES],
   exports: [TopbarComponent],
 })
--- frontend/src/app/dashboard/dashboard.routes.ts
+++ frontend/src/app/dashboard/dashboard.routes.ts
@@ -1,11 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { UserComponent } from './views/user/user.component';
+import { ClientsComponent } from './views/clients/clients.component';
+import { CompaniesComponent } from './views/companies/companies.component';
+import { InvoicesComponent } from './views/invoices/invoices.component';
+import { OffersComponent } from './views/offers/offers.component';
+import { LogsComponent } from './views/logs/logs.component';
+import { ConfigComponent } from './views/config/config.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'users',
+      },
+      {
+        path: 'users',
+        component: UserComponent,
+      },
+      {
+        path: 'clients',
+        component: ClientsComponent,
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'invoices',
+        component: InvoicesComponent,
+      },
+      {
+        path: 'offers',
+        component: OffersComponent,
+      },
+      {
+        path: 'logs',
+        component: LogsComponent,
+      },
+      {
+        path: 'config',
+        component: ConfigComponent,
+      },
+    ],
   },
 ];
 
