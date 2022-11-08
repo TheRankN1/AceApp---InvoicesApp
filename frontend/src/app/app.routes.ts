def7f97e59a03c0058ab05c67da49c67f2bdc2af
TheRankN1 <petrutpoenaru@yahoo.com>
2022-11-08 16:54:09 +0200

    added the UI for users table

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -17,6 +17,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
+  // {
+  //   path : '**',
+  //   component ; "TO DO HERE"
+  //
+  // }
 ];
 
 @NgModule({
