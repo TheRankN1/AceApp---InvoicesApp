49bdade4cbf60b3206583e5ef6fa19da5074c56b
TheRankN1 <petrutpoenaru@yahoo.com>
2022-11-21 20:31:21 +0200

    updated schema for contract

--- api/app/modules/contract/contract.schema.js
+++ api/app/modules/contract/contract.schema.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const ContractSchema = mongoose.Schema({
   no: String,
-  issuedAt: Date,
+  issuedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const ContractCollection = mongoose.model("Contract", ContractSchema);
