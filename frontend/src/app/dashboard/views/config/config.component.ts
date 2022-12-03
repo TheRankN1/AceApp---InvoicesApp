a226c37c4fb7c621b911db492b49c1818d0db45b
TheRankN1 <petrutpoenaru@yahoo.com>
2022-12-03 15:53:47 +0200

    REFACTORY COLORS AND SISEZ WITH VARIABLES

--- frontend/src/app/dashboard/views/config/config.component.ts
+++ frontend/src/app/dashboard/views/config/config.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CompanyInterface } from '../../../shared/interfaces/company.interface';
+import { ConfigService } from '../../services/config.service';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.scss'],
 })
-export class ConfigComponent {}
+export class ConfigComponent {
+  public configFormGroup: FormGroup;
+
+  constructor(private fb: FormBuilder, private configService: ConfigService) {
+    this.configFormGroup = this.formBuilder();
+  }
+
+  public formBuilder() {
+    return (this.configFormGroup = this.fb.group({
+      batchNumbers: this.fb.control(''),
+
+      ron: this.fb.control(''),
+      euro: this.fb.control(''),
+      usd: this.fb.control(''),
+
+      // company: this.fb.control(''),
+    }));
+  }
+  public createConfigs() {
+    const body = this.configFormGroup.value;
+
+    this.configService.createConfig(body).subscribe((respone) => {
+      console.log(respone);
+    });
+  }
+}
